ec1f2660aa026ddcc82587799f5327be9e8948f9
Spiro Stameson <sstameso@caltech.edu>
2023-01-18 11:29:57 -0800

    Update constant generation

--- hw1/gen_consts.py
+++ hw1/gen_consts.py
@@ -2,19 +2,40 @@ import numpy as np
 
 
 def float_to_fixed(x: float, NUM_FRAC=18, NUM_BITS=22) -> str:
+    if not np.isfinite(x):
+        x = 0
     return f'{round(x * (1 << NUM_FRAC)) % (1 << NUM_BITS):0{NUM_BITS}b}'
 
 def fixed_to_float(x: int, NUM_FRAC=18) -> float:
     return x / (1 << NUM_FRAC)
 
-K = 0.6072529350
+K0 = 1
+K1 = 1 / np.sqrt(2)
+K2 = 1
+for i in range(1, 1000):
+    a = 1 / (1 << i)
+    K1 *= 1 / np.sqrt(1 + a**2)
+    K2 *= 1 / np.sqrt(1 - a**2)
 
-print( 'constant K: std_logic_vector(21 downto 0) :=')
-print(f'    "{float_to_fixed(K)}";')
+# K1 = 0.6072529350
+print(f'K(0) = {K0}')
+print(f'K(1) = {K1}')
+print(f'K(2) = {K2}')
 
-print('constant consts: calc_vector(0 to 15) := (')
+print( 'constant K: <T> :=')
+print('    (', end="")
+print(f'"{float_to_fixed(K0)}", ', end="")
+print(f'"{float_to_fixed(K1)}", ', end="")
+print(f'"{float_to_fixed(K2)}");')
+
+print('constant consts: <T> := (')
 for i in range(16):
-    print(f'    "{float_to_fixed(np.arctan(1 / (1 << i)))}",')
+    a = 1 / (1 << i)
+    print('    (', end="")
+    print(f'"{float_to_fixed(a)}", ', end="")
+    print(f'"{float_to_fixed(np.arctan(a))}", ', end="")
+    print(f'"{float_to_fixed(np.arctanh(a))}"', end="")
+    print('),')
 print(');')
 
 print(f'x = {float_to_fixed(28 * np.pi / 180, NUM_FRAC=14, NUM_BITS=16)}')
